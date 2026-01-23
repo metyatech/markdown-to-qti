2abe608dc4d90180115b9d06b68d372cfb0c5302
metyatech <metyatech@gmail.com>
2026-01-23 21:26:24 +0900

    chore: trim unused Gradle settings

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,12 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     kotlin("jvm") version "2.1.0"
+}
+
+kotlin {
+    jvmToolchain(23)
 }
 
 repositories {
@@ -9,9 +16,15 @@ repositories {
 dependencies {
     val junitVersion = "6.0.2"
     testImplementation(platform("org.junit:junit-bom:$junitVersion"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.junit.jupiter:junit-jupiter-api")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile>().configureEach {
+    compilerOptions.jvmTarget.set(JvmTarget.JVM_23)
 }
